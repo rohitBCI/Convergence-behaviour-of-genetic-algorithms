af732e25aff14e25a7e54bbf1c453681922bdf10
Rohit Vijayakumar <rohitmacx@gmail.com>
2021-02-25 10:11:43 +0100

    Added unifrom crossover

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -1,7 +1,7 @@
 import numpy as np
 import random
-class Genetic_Algorithm():
 
+class ga():
     def __init__(self, population, individuals, chromosome_length,scores,parent_1,parent_2, mutation_probability):
     	self.population = population
     	self.individuals= individuals
@@ -61,6 +61,23 @@ class Genetic_Algorithm():
         return child_1, child_2
 
 
+
+    def uniform_crossover(parent_1, parent_2):
+    	chromosome_length = len(parent_1)
+    	child_1 = np.array([])
+    	child_2 = np.array([])
+    	for i in range(chromosome_length):
+    		if(parent_1[i]==parent_2[i]):
+    			child_1  = np.hstack((child_1,parent_1[i]))
+    			child_2  = np.hstack((child_2,parent_1[i]))       
+    		else:
+    			random_sample1 = random.randint(0, 1)
+    			child_1  = np.hstack((child_1,random_sample1))
+    			random_sample2 = random.randint(0, 1)
+    			child_2  = np.hstack((child_2,random_sample2))
+    	return child_1, child_2
+
+
     def check_stopping_criterion(fitness_values):
     	parent1_fitness = fitness_values[0]
     	parent2_fitness = fitness_values[1]
@@ -78,3 +95,47 @@ class Genetic_Algorithm():
 
     	else:
     		return False
+
+    def run_generation(population):
+    	num_generation = 0
+    	best_score_progress = []
+    	while(True):
+		    num_generation += 1
+		    flag = 1
+		    population_size = len(population) 
+		    chromosome_length = len(population[0])
+		    # Create an empty list for new population
+		    new_population = np.array([]).reshape(0, chromosome_length)
+		    # Create new popualtion generating two children at a time
+		    for i in range(0,population_size-1,2):
+		        parent_1 = population[i]
+		        parent_2 = population[i+1]
+		        child_1, child_2 = ga.uniform_crossover(parent_1, parent_2)
+		        family = np.array([parent_1,parent_2,child_1,child_2])
+		        family_fitness = ga.calculate_fitness(family)
+		        #print(family_fitness,flag)
+		        stopping_criterion  = ga.check_stopping_criterion(family_fitness)
+		        if(stopping_criterion == False):
+		            flag=0
+		        best_two = np.argsort(family_fitness)[::-1][:2]
+		        best_in_family = family[best_two]
+		        new_population =  np.vstack((new_population, best_in_family))
+		        
+		    # Replace the old population with the new one
+		    population = np.array(new_population)
+		    
+		    # Score best solution, and add to tracker
+		    scores = ga.calculate_fitness(population)
+		    best_score = np.max(scores)/chromosome_length * 100
+		    best_score_progress.append(best_score)
+		    if(best_score==100):
+		        return (population[np.argmax(scores)])
+		    if(flag==1):
+		        return "Fail"
+		#     else:
+		#         print(f'Generation : {num_generation}, score : {best_score}')
+		# GA has completed required generation
+		#print ('End best score, percent target: %.1f' %best_score)
+		
+		    
+		    
